767d5f81b125a82ee93c053d4759db2604ed81a3
Andy Enkeboll <>
2022-03-03 19:11:04 -0500

    lecture 5

--- lectures/wc.py
+++ lectures/wc.py
@@ -1,14 +1,23 @@
 from mrjob.job import MRJob
+import csv
 
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
-        yield "chars", len(line)
-        yield "words", len(line.split())
-        yield "lines", 1
+        reader = csv.reader([line])
+        for line in reader:
+            if line.startswith('business_id'):
+                return
+            yield 'num_words', len(line[4].split())
+
 
     def reducer(self, key, values):
-        yield key, sum(values)
+        total_words = 0
+        total_reviews = 0
+        for val in values:
+            total_words += val
+            total_reviews += 1
+        yield key, sum(total_words) / total_reviews
 
 
 if __name__ == '__main__':
